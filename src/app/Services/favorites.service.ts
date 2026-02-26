59f28962f5f07b994cf4ed46beca5abde854bee7
Poutou <dutertre.adrien@pm.me>
2026-02-26 11:29:35 +0100

    Finalisation des favoris plus  un peu de UI

--- src/app/Services/favorites.service.ts
+++ src/app/Services/favorites.service.ts
@@ -1,20 +1,30 @@
 import { computed, Injectable, signal } from '@angular/core';
+import { favoriteWork } from './Interface/utils.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FavoritesService {
-  favorites = signal<string[]>([]);
+  favorites = signal<favoriteWork[]>([]);
   readonly count = computed(() => {
     return this.favorites()
                .length
   });
 
+  // Créer un objet favoris
+  newFavorite(newKey: string, newTitle: string, newPhoto: number): favoriteWork {
+    return {
+      key: newKey,
+      title: newTitle,
+      photo: newPhoto
+    }
+  }
+
   // Ajouter aux favoris
-  addToFavorites(workKey: string): void {
+  addToFavorites(newFavorite: favoriteWork): void {
     this.favorites
         .update((favoritesWorks) => {
-          return [...favoritesWorks, workKey]
+          return [...favoritesWorks, newFavorite]
         });
     this.saveFavorites();
   }
@@ -22,15 +32,15 @@ export class FavoritesService {
   // Vérifie l'appartenance aux favoris
   isFavorite(workKey: string): boolean {
     return this.favorites()
-               .includes(workKey);
+               .some((favorite: favoriteWork) => favorite.key === workKey);
   }
 
   // Supprimer des favoris
   removeFavorite(workKey: string) : void {
     this.favorites
-        .update((favoritesWorks) => {
-          const filteredFavorites: string[] = favoritesWorks.filter((work: string) => {
-            return work != workKey;
+        .update((favoritesWorks: favoriteWork[]) => {
+          const filteredFavorites: favoriteWork[] = favoritesWorks.filter((work: favoriteWork) => {
+            return work.key != workKey;
           });
 
           return [...filteredFavorites]
@@ -40,15 +50,18 @@ export class FavoritesService {
 
   // Enregistrer les favoris dans le localStorage
   saveFavorites(): void {
-    localStorage.setItem("favorites", this.favorites().join(";"));
+    localStorage.setItem("favorites", JSON.stringify(this.favorites()));
   }
 
   // Récupérer les favoris dans le localStorage
   loadFavorites(): void {
-    const loadedFavorites: string = localStorage.getItem("favorites") ?? "";
-    this.favorites
-        .set(
-          loadedFavorites.split(";")
-        );
+    if (localStorage.getItem("favorites")) {
+      const loadedFavorites: string = localStorage.getItem("favorites") ?? "";
+       this.favorites
+           .set(
+            JSON.parse(loadedFavorites)
+           );
+    }
+    
   }
 }
